6a619e401c4b4f752e466d84e92ebcf3a8ff75d8
Huzaifa Subhani <huzaifasubhani.hs@example.com>
2024-12-11 18:11:48 +0500

    Created new api for Key performance Indicator

--- Extraction/urls.py
+++ Extraction/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('api/', include('financial_market_data.urls')),
     path('api/', include('Economic_Data.urls')),
     path('api/', include('financial_statement_analysis.urls')),
+    path('api/', include('key_performance_indicator.urls')),
     path('api/', include('Altman_Zscore.urls')),
     path('api/', include('GMM.urls')),
 
