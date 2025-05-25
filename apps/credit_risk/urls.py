fa2c36d00f9e1e8aa329af458df22bc2e1b1284b
HuzaifaSubhani <huzaifasubhani.hs@gmail.com>
2025-05-25 17:32:28 +0500

    updated Urls.py of root as well as credit risk

--- apps/credit_risk/urls.py
+++ apps/credit_risk/urls.py
@@ -12,20 +12,20 @@ from .views.overall_score_view import OverallScoreView
 
 urlpatterns = [
     # Endpoints for Organization Data (Logic One)
-    path('organization/fetch-specific-org/', FetchSpecificOrgDataView.as_view(), name='fetch_specific_org_data'),
-    path('organization/fetch-by-sector/', FetchSectorDataView.as_view(), name='fetch_by_sector_data'),
+    path('credit-risk/organization/fetch-specific-org/', FetchSpecificOrgDataView.as_view(), name='fetch_specific_org_data'),
+    path('credit-risk/organization/fetch-by-sector/', FetchSectorDataView.as_view(), name='fetch_by_sector_data'),
 
     # Endpoints for Financial Variables (Logic Two)
-    path('financial-variables/org-specific/', OrgSpecificFinancialVariablesView.as_view(), name='org_specific_financial_variables'),
-    path('financial-variables/sector-average/', SectorAverageFinancialVariablesView.as_view(), name='sector_average_financial_variables'),
+    path('credit-risk/financial-variables/org-specific/', OrgSpecificFinancialVariablesView.as_view(), name='org_specific_financial_variables'),
+    path('credit-risk/financial-variables/sector-average/', SectorAverageFinancialVariablesView.as_view(), name='sector_average_financial_variables'),
 
     # Endpoints for Financial Ratios (Logic Three)
-    path('financial-ratios/company-ratios/', CompanyRatiosView.as_view(), name='company_ratios'),
-    path('financial-ratios/sector-ratios/', SectorRatiosView.as_view(), name='sector_ratios'),
+    path('credit-risk/financial-ratios/company-ratios/', CompanyRatiosView.as_view(), name='company_ratios'),
+    path('credit-risk/financial-ratios/sector-ratios/', SectorRatiosView.as_view(), name='sector_ratios'),
 
     # Endpoints for Ratio Comparison (Logic Four)
-    path('ratio-comparison/compare-ratios/', RatioComparisonView.as_view(), name='compare_ratios'),
+    path('credit-risk/ratio-comparison/compare-ratios/', RatioComparisonView.as_view(), name='compare_ratios'),
 
     # Endpoints for Overall Score (Logic Five)
-    path('overall-score/calculate/', OverallScoreView.as_view(), name='calculate_overall_score'),
+    path('credit-risk/overall-score/calculate/', OverallScoreView.as_view(), name='calculate_overall_score'),
 ]
